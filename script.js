aaad04681216bf3ad79b3dc9701239fe3fff20aa
Mustafa <mustafamcm9@gmail.com>
2022-12-23 16:34:17 +0300

    new commit

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const refreshBtn = document.getElementById('refresh-btn');
 const scoreEl = document.getElementById('score');
 const startBtn = document.getElementById('start-btn');
 const timeEl = document.getElementById('time');
+const divs = document.querySelectorAll('div'); // All divs
 
 const src = [ // Images src array
   "images/pic1.png",
@@ -21,36 +22,22 @@ const src = [ // Images src array
   "images/pic6.png"
 ];
 
-let arr = [];
-let clickArr = [];
+let arr = []; // Store clicked images
+let clickArr = []; // Store clicked divs
 let moves = 0;
 let score = 0;
 let clickCount = 0;
 
-function shuffleArray(array) { // Shuffle array function (Study This)
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-shuffleArray(src); // Shuffle the array
 
 
-const startGame =() => {
-  
-  startContainer.style.display = 'none';
-  mainContainer.style.display = 'flex'
-
-}
 
 
-const divs = document.querySelectorAll('div');
-
-for(let i = 0; i < divs.length; i++) { // Loop the cards
-
-
+for(let i = 0; i < divs.length; i++) { // Loop the cards(divs)
 
   divs[i].addEventListener('click',function(){ // Add event listener to every card
+
+   turnCardFront(divs[i],src[i]); // Turn the cards front side
+
     clickCount++
 
     if(clickCount === 1) {
@@ -90,37 +77,26 @@ for(let i = 0; i < divs.length; i++) { // Loop the cards
     }
     
 
-    divs[i].innerHTML = // Display cards at the divs
-    `
-    <img class="card-img" src="${src[i]}" alt="">
-  `
-
-    clickArr.push(divs[i])
-    arr.push(src[i]);
+    clickArr.push(divs[i]) // Push the last clicked div(card) (So you can change their inner html)
+    arr.push(src[i]); // Push the last clicked img
     
+
     if(arr.length === 2) { // If user picks 2 cards
       if(arr[arr.length -1] === arr[arr.length -2]){ // If cards are same
         arr = [] // Reset the array
         score ++
-        scoreEl.textContent = `score: ${score}`
-      } else{
-        let timeoutId = setTimeout(function() { // 1 second delay 
+        scoreEl.textContent = `score: ${score}` // Reflect the score
+
+      } else{ // If cards are not same
+        setTimeout(function() { // 750 ms delay 
           arr = [] // Reset the array
-          clickArr[clickArr.length - 1].innerHTML = `
-          <figure>
-          <img class="card" src="images/logo.png" alt="">
-        </figure>
-          ` // Turn cards 
-          clickArr[clickArr.length - 2].innerHTML = `
-          <figure>
-          <img class="card" src="images/logo.png" alt="">
-        </figure>
-          `// Turn cards 
-        }, 1000);
+          turnCardBack(clickArr[clickArr.length -1]); // Turn cards back side
+          turnCardBack(clickArr[clickArr.length -2]); 
+        }, 750);
       }
     } else if(arr.length > 2){ // If player try to pick more than two cards at the same time
       arr = []
-      refresh()
+      refresh();
       movesEl.textContent = 'PICK 2 CARDS MAX AT ONE MOVE'
     }
     
@@ -130,6 +106,37 @@ for(let i = 0; i < divs.length; i++) { // Loop the cards
 
 
 
+const shuffleArray = (array) => { // Shuffle array function (Study This)
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+shuffleArray(src); // Shuffle the array
+
+
+const startGame =() => { // Start game functipn
+  
+  startContainer.style.display = 'none';
+  mainContainer.style.display = 'flex'
+
+}
+
+
+const turnCardBack = (val) => { // Turn cards backside function
+  val.innerHTML = `
+  <figure>
+  <img class="card" src="images/logo.png" alt="">
+</figure>
+  `
+}
+
+
+const turnCardFront = (val,arr) => { // Turn cards frontside function
+  val.innerHTML = `
+  <img class="card-img" src="${arr}" alt="">
+`
+}
 
 
 const refresh = () => { // Refresh Function
@@ -138,6 +145,7 @@ const refresh = () => { // Refresh Function
   moves = 0;
   matchCount = 0;
   score = 0;
+  clickCount === 0;
   movesEl.textContent = `${moves} moves`;
   scoreEl.textContent = 'Score: '
   for(let i = 0; i < divs.length; i++) {
@@ -150,4 +158,3 @@ const refresh = () => { // Refresh Function
   shuffleArray(src);
   console.clear()
 }
-
